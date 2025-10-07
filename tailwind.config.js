46ecb75847ed2189d4e9584ecbc1f6622a94c24d
paramitathecdrom <gparamita576@gmail.com>
2025-10-07 16:59:44 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: "#2563eb", // blue-600
-        accent: "#fbbf24", // amber-400
-        dark: "#0f172a", // slate-900
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
